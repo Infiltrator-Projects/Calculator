e92e975981a0443ea06e3b05ddbbc23b1e1218e1
Shannon Smith <infiltratr@yandex.com>
2026-09-24 12:10:06 +1000

    Test Programmer byte swap and grouping

--- tests/test_programmer.cpp
+++ tests/test_programmer.cpp
@@ -35,6 +35,10 @@ int main(){
     expect_ok("0f nand 03",ProgrammerBase::Hexadecimal,IntegerWidth::Bits8,0xfc);
     expect_ok("0f nor f0",ProgrammerBase::Hexadecimal,IntegerWidth::Bits8,0x00);
     expect_ok("1 rol 9",ProgrammerBase::Binary,IntegerWidth::Bits8,2);
+    expect_ok("bswap 12",ProgrammerBase::Hexadecimal,IntegerWidth::Bits8,0x12);
+    expect_ok("bswap 1234",ProgrammerBase::Hexadecimal,IntegerWidth::Bits16,0x3412);
+    expect_ok("bswap 12345678",ProgrammerBase::Hexadecimal,IntegerWidth::Bits32,0x78563412);
+    expect_ok("bswap 0123456789abcdef",ProgrammerBase::Hexadecimal,IntegerWidth::Bits64,0xefcdab8967452301ULL);
     expect_ok("~0",ProgrammerBase::Decimal,IntegerWidth::Bits8,255);
     expect_ok("0xff + 1",ProgrammerBase::Hexadecimal,IntegerWidth::Bits8,0);
     expect_ok("ff & 0f",ProgrammerBase::Hexadecimal,IntegerWidth::Bits8,15);
@@ -66,6 +70,19 @@ int main(){
     if(reps.octal!="377") fail("representation octal wrong");
     if(reps.decimal!="-1") fail("representation signed decimal wrong");
     if(reps.hexadecimal!="FF") fail("representation hexadecimal wrong");
+    if(calculator::group_programmer_digits(
+           "1111000011110000", ProgrammerBase::Binary) !=
+       "1111 0000 1111 0000") {
+        fail("binary digit grouping wrong");
+    }
+    if(calculator::group_programmer_digits(
+           "1234567", ProgrammerBase::Octal) != "1 234 567") {
+        fail("octal digit grouping wrong");
+    }
+    if(calculator::group_programmer_digits(
+           "DEADBEEF", ProgrammerBase::Hexadecimal) != "DEAD BEEF") {
+        fail("hexadecimal digit grouping wrong");
+    }
 
     if(failures){std::cerr<<failures<<" programmer test(s) failed\n";return 1;}
     std::cout<<"programmer tests passed\n";
